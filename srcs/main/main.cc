829dc311f34914699d1c6a267a4aecff06d08217
xwu36 <xwu36@buffalo.edu>
2021-11-13 16:31:07 -0800

    Create two threads to run matching_engine daily

--- srcs/main/main.cc
+++ srcs/main/main.cc
@@ -1,6 +1,10 @@
+#include <condition_variable>
+#include <chrono>
 #include <iostream>
 #include <fstream>
+#include <mutex>
 #include <string>
+#include <thread>
 #include <vector>
 
 #include "absl/flags/flag.h"
@@ -33,6 +37,100 @@ ABSL_FLAG(std::string, order_file_path_prefix, "srcs/main/data/", "Directory whi
 ABSL_FLAG(std::string, order_file_debug_path, "srcs/main/data/orders.jsonl",
           "Input order path for debugging purpose");
 
+std::mutex g_mutex;
+std::condition_variable g_cv;
+bool g_is_market_on = false;
+int g_data = 0;
+
+void MarketStatusChecker()
+{
+  absl::TimeZone utc = absl::UTCTimeZone();
+  while (true)
+  {
+    std::this_thread::sleep_for(std::chrono::seconds(60));
+
+    const std::string now_str = absl::FormatTime("%H:%M", absl::Now(), utc);
+    LOG(INFO) << "Checking market status at " << now_str << "...";
+
+    std::unique_lock<std::mutex> ul(g_mutex);
+
+    g_is_market_on = (now_str == "09:00");
+
+    ul.unlock();
+
+    if (!g_is_market_on)
+    {
+      continue;
+    }
+    LOG(INFO) << "Market is open!";
+
+    g_cv.notify_one();
+    ul.lock();
+    g_cv.wait(ul, []()
+              { return g_is_market_on == false; });
+  }
+}
+
+void RunMatchingEngine(MatchingEngineImpl &matching_engine, const MarketDataPublisherImpl &market_data_publiser)
+{
+  absl::TimeZone utc = absl::UTCTimeZone();
+  while (true)
+  {
+    LOG(INFO) << "Start processing offer for today...";
+    std::unique_lock<std::mutex> ul(g_mutex);
+
+    g_cv.wait(ul, []()
+              { return g_is_market_on == true; });
+
+    g_is_market_on = false;
+
+    ul.unlock();
+
+    g_cv.notify_one();
+    ul.lock();
+
+    const std::string now_str = absl::FormatTime("%Y-%m-%d/", absl::Now(), utc);
+    const std::string one_day_ago_str = absl::FormatTime("%Y-%m-%d/", absl::Now() - absl::Hours(24), utc);
+    const std::string order_file_path_prefix = absl::GetFlag(FLAGS_order_file_path_prefix);
+    const std::string orders_to_process_today = absl::StrCat(order_file_path_prefix, now_str, "orders.jsonl");
+    const std::string gtc_orders_today = absl::StrCat(
+        order_file_path_prefix, now_str, kGtcOrdersFileName);
+    const std::string gtc_orders_a_day_ago = absl::StrCat(
+        order_file_path_prefix, one_day_ago_str, kGtcOrdersFileName);
+    LOG(INFO) << "The path of all offers to be processed today: " << orders_to_process_today;
+    LOG(INFO) << "The path of unmatched GTC offers for today: " << gtc_orders_today;
+    LOG(INFO) << "The path of unmatched GTC offers from yesterday: " << gtc_orders_a_day_ago;
+
+    // Read yesterday's unmatched GTC orders.
+    absl::Status init_stats = matching_engine.InitOnMarketStarts(gtc_orders_a_day_ago);
+    if (!init_stats.ok())
+    {
+      LOG(ERROR) << init_stats.message();
+    }
+
+    // Process today's orders.
+    std::vector<Order> orders = fep::srcs::order::ReadOrdersFromPath(absl::GetFlag(FLAGS_order_file_debug_path));
+    // Loop through all the offers and process each of them.
+    for (const auto &order : orders)
+    {
+      absl::StatusOr<TradeMessage> message = matching_engine.Process(std::make_unique<Order>(order));
+      if (message.ok())
+      {
+        market_data_publiser.Publish(message.value());
+      }
+      else
+      {
+        LOG(ERROR) << message.value().to_str() << std::endl;
+      }
+
+      // Clear the matching engine and output today's unmatched GTC orders.
+      absl::Status clear_status = matching_engine.ClearOnMarketEnds(gtc_orders_today);
+    }
+
+    LOG(INFO) << "Market is closed!";
+  }
+}
+
 int main(int argc, char *argv[])
 {
   // google::InitGoogleLogging(argv[0]);
@@ -52,42 +150,9 @@ int main(int argc, char *argv[])
   const MarketDataPublisherFactory market_data_publiser_factory;
   const MarketDataPublisherImpl market_data_publiser = market_data_publiser_factory.ProduceMarketDataPublisher();
 
-  absl::TimeZone utc = absl::UTCTimeZone();
-  const std::string now_str = absl::FormatTime("%Y-%m-%d/", absl::Now(), utc);
-  const std::string one_day_ago_str = absl::FormatTime("%Y-%m-%d/", absl::Now() - absl::Hours(24), utc);
-  const std::string order_file_path_prefix = absl::GetFlag(FLAGS_order_file_path_prefix);
-  const std::string orders_to_process_today = absl::StrCat(order_file_path_prefix, now_str, "orders.jsonl");
-  const std::string gtc_orders_today = absl::StrCat(
-      order_file_path_prefix, now_str, kGtcOrdersFileName);
-  const std::string gtc_orders_a_day_ago = absl::StrCat(
-      order_file_path_prefix, one_day_ago_str, kGtcOrdersFileName);
-  LOG(INFO) << "The path of all offers to be processed today: " << orders_to_process_today;
-  LOG(INFO) << "The path of unmatched GTC offers for today: " << gtc_orders_today;
-  LOG(INFO) << "The path of unmatched GTC offers from yesterday: " << gtc_orders_a_day_ago;
+  std::thread market_status_thread(MarketStatusChecker);
+  std::thread run_matching_engine_thread(RunMatchingEngine, std::ref(matching_engine), std::ref(market_data_publiser));
 
-  // Read yesterday's unmatched GTC orders.
-  absl::Status init_stats = matching_engine.InitOnMarketStarts(gtc_orders_a_day_ago);
-  if (!init_stats.ok())
-  {
-    LOG(ERROR) << init_stats.message();
-  }
-
-  // Process today's orders.
-  std::vector<Order> orders = fep::srcs::order::ReadOrdersFromPath(absl::GetFlag(FLAGS_order_file_debug_path));
-  // Loop through all the offers and process each of them.
-  for (const auto &order : orders)
-  {
-    absl::StatusOr<TradeMessage> message = matching_engine.Process(std::make_unique<Order>(order));
-    if (message.ok())
-    {
-      market_data_publiser.Publish(message.value());
-    }
-    else
-    {
-      LOG(ERROR) << message.value().to_str() << std::endl;
-    }
-  }
-
-  // Clear the matching engine and output today's unmatched GTC orders.
-  absl::Status clear_status = matching_engine.ClearOnMarketEnds(gtc_orders_today);
+  market_status_thread.join();
+  run_matching_engine_thread.join();
 }
